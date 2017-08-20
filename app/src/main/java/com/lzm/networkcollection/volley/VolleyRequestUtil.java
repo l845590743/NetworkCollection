a69b5260832f753f45e422dbb0bdeb044fbf8cd4
zhimin.liu <zhimin8090186@163.com>
2017-08-20 11:53:37 +0800

    feat: Volley bug
    Signed-off-by: zhimin.liu <zhimin8090186@163.com>

--- app/src/main/java/com/lzm/networkcollection/volley/VolleyRequestUtil.java
+++ app/src/main/java/com/lzm/networkcollection/volley/VolleyRequestUtil.java
@@ -33,7 +33,16 @@ public class VolleyRequestUtil {
         // 为当前请求添加标记
         stringRequest.setTag(tag);
 
-        stringRequest.setRetryPolicy(new DefaultRetryPolicy(2000,DefaultRetryPolicy.DEFAULT_MAX_RETRIES,DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        /**
+         *     重复请求的问题
+         * 1.DefaultRetryPolicy实现于RetryPolicy,RetryPolicy是一个接口;
+         2.DefaultRetryPolicy构造函数中的参数1是设置超时时长,默认为2500，可以设置稍微长点；
+         3.DefaultRetryPolicy构造函数中的参数2是设置最大重复请求次数，默认为1，可以设置为0；
+         4.DefaultRetryPolicy构造函数中的参数3为设置 “允许你指定一个退避乘数可以用来 实现<指数退
+         避>来从RESTful服务器请求数据”,默认值为1，当取1时，即可以简单理解为 ” 每次超时请求时长都是 <前一次超时请求时长 *2> “，以此类推
+         */
+
+        stringRequest.setRetryPolicy(new DefaultRetryPolicy(20 * 1000,0,DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
         // 将当前请求添加到请求队列中
         MainActivity.getRequestQueue().add(stringRequest);
         // 重启当前请求队列
